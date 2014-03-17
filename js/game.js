9400c2713c914ba1d2052f5c57311379479ac4f1
jongmini <jongmin.choi@gmail.com>
2014-03-16 19:20:04 -0700

    some of the basic functions are connected to the click function

--- js/game.js
+++ js/game.js
@@ -3,9 +3,9 @@ var ticTacToeApp = angular.module("ticTacToeApp", []);
 ticTacToeApp.controller('ticTacToeCtrl',['$scope', function($scope){
   $scope.game              = $('#game');   // the game container
   $scope.board             = $('#board');   // the board  container
-  // $scope.status_indicators = $('#teams li');   // status bar container
+  $scope.statusIndicators = $('#teams li');   // status bar container
 
-  // $scope.tiles = [];                         // all the "tiles"
+  $scope.tiles = [];                         // all the "tiles"
 
   $scope.players = [                        
     {
@@ -20,6 +20,12 @@ ticTacToeApp.controller('ticTacToeCtrl',['$scope', function($scope){
     }
   ];
 
+  $scope.initialize = function(){
+
+  $scope.win_combos = [
+    [0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]
+  ];
+
   $scope.tiles = [
     {id: "0", isActive: false, marker:''}, 
     {id: "1", isActive: false, marker:''},
@@ -31,7 +37,29 @@ ticTacToeApp.controller('ticTacToeCtrl',['$scope', function($scope){
     {id: "7", isActive: false, marker:''},
     {id: "8", isActive: false, marker:''}
     
-  ];
+    ];
+
+  };
+
+  $scope.activePlayer = function(){
+    if ($scope.turns % 2 === 1 ){
+      $scope.currentPlayer = $scope.players[0];
+    } else {
+      $scope.currentPlayer = $scope.players[1];
+    }
+
+  };
+
+  $scope.markSq = function(id){
+    $scope.activePlayer();
+    if($scope.tiles[id].team === ""){
+      $scope.tiles[id].team = $scope.currentPlayer.marker;
+      $scope.turns++;
+    } else {
+      console.log("taken");
+    }
+    console.log($scope.turns);
+  };
 
   $scope.handleClick = function() {
     console.log(this.$index);
@@ -47,6 +75,8 @@ ticTacToeApp.controller('ticTacToeCtrl',['$scope', function($scope){
 
   };
 
+ 
 
+$scope.initialize();
 }]);
 
